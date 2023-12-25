07ea95e5b27dd78e769c8cc38ff38ff00af63b00
Akashram28 <akashram288@gmail.com>
2023-12-25 13:41:21 +0530

    Added Tournamnet and Roulette Selection

--- Optimizer.py
+++ Optimizer.py
@@ -2,14 +2,17 @@ from sklearn.model_selection import KFold, cross_val_score
 from individual import Individual
 from sklearn.base import clone
 import random
+import bisect
 
 class Optimizer:
-    def __init__(self,X,y,model,ranges,population=10,n_splits = 5,selection_chance=0.5):
+    def __init__(self,X,y,model,ranges,selection="both",population=10,n_splits = 5,selection_chance=0.5,tournament_split=2):
         self.kfold = KFold(n_splits=n_splits, shuffle=True, random_state=42)
         self.X = X
         self.y = y
         self.model = model
         self.ranges = ranges
+        self.selection = selection
+        self.tournament_split = tournament_split
         self.population = population
         self.selection_chance = selection_chance
         self.indis = []
@@ -50,15 +53,40 @@ class Optimizer:
     def sort_indis(self):
         indis = self.indis
         for i in indis:
-            i.score = self.getScore(i)
+            if i.score == -1:
+                i.score = self.getScore(i)
         indis.sort(key=lambda x: x.score)
         self.indis = indis
         sum_score = sum((i.score for i in indis))
         roulette_probs = [i.score/sum_score for i in indis]
+        roulette_prob_sum = 0
+        for i in range(len(roulette_prob_sum)):
+            roulette_prob_sum += roulette_probs[i]
+            roulette_probs[i] = roulette_prob_sum
         self.roulette_probs = roulette_probs
     
+    def tournament(self):
+        tournament_indis = random.sample(range(len(self.indis)),self.tournament_split)
+        tournament_indis.sort()
+        selected_indi_1 = self.indis[tournament_indis[-1]]
+        selected_indi_2 = self.indis[tournament_indis[-2]]
+        return selected_indi_1,selected_indi_2
+    
+    def roulette_wheel(self):
+        selected_indi_1 = self.indis[bisect.bisect_left(self.roulette_probs,random.random())]
+        selected_indi_2 = self.indis[bisect.bisect_left(self.roulette_probs,random.random())]
+        return selected_indi_1,selected_indi_2
+    
     def select(self):
-        pass
+        if self.selection == 'tournament':
+            self.tournament()
+        elif self.selection == 'roulette':
+            self.roulette_wheel()
+        else:
+            if random.random() < self.selection_chance:
+                self.tournament()
+            else:
+                self.roulette_wheel()
 
     def crossover(self):
         pass
